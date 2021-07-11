8ce382f96ecdf49ec8d1c2ae1c1af498941d81a0
Maha420 <81813193+Maha420@users.noreply.github.com>
2021-07-11 08:34:52 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -6,8 +6,8 @@ $message= $_POST['message'];
 $to = "meme.wow.11@gmail.com";
 $subject = "Mail From website";
 $txt ="Name = ". $name . "\r\n  Email = " . $email . "\r\n Message =" . $message;
-$headers = "From: noreply@Maha420.com" . "\r\n" .
-"CC: somebodyelse@example.com";
+$headers = "From: noreply@maha420.github.io" . "\r\n" .
+;
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
